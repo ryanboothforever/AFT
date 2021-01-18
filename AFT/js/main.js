e0f451ba6bbf204053a3b4ed49b885fa99ff8260
yobee333 <69943959+yobee333@users.noreply.github.com>
2021-01-18 16:14:02 -0600

    Add files via upload

---
+++ AFT/js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('#go-inventory').addEventListener('click', eBay)
+
+function eBay(){
+    alert('Please click on the "eBay" button on the bottom right to view that truck\'\s eBay listing')
+}
