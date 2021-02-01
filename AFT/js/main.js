636346a02703ac276f564c98a0517dbd30e045f3
yobee333 <69943959+yobee333@users.noreply.github.com>
2021-02-01 14:03:13 -0600

    Add files via upload

--- AFT/js/main.js
+++ AFT/js/main.js
@@ -4,5 +4,12 @@ function eBay(){
     alert('Please click on the "eBay" button on the bottom right to view that truck\'\s eBay listing')
 }
 
-
+function toHamburger() {
+    let x = document.getElementById("myNavBar");
+    if (x.className === "nav-bar") {
+    x.className += " responsive";
+    } else {
+    x.className = "nav-bar";
+    }
+}
 
